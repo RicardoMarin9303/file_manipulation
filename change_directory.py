0753c07eafa297d3db1fdba370f56882e671b928
Ricardo <rlmz0327@gmail.com>
2024-02-07 22:54:22 +0100

    first commit for the .py file, add existing file

---
+++ change_directory.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Proyecto que tiene la finalidad de manipular ciertos ficheros bajo el lenguaje de programacion python
+"""
+
+# Importamos librerias
+import os
+# Variables
+directory = os.getcwd()
+# Declaramos nuestra funcion
+def change_name(directory, signo):
+# lista todo los ficheros y directorios
+    for i in os.listdir(directory):
+# Si es un directorio, continua
+        if os.path.isdir(i):
+# almacenamos en un string nuestro nombre
+            replace_name = i.replace(" ", signo)
+            path_name = os.path.join(directory, i)
+            path_replace = os.path.join(directory, replace_name)
+            os.rename(path_name, path_replace)
+        else:
+            print("En el siguiente fichero, " + i + " tienes un error")
+
+
+# inicializamos la funcion
+change_name(directory, "_")
